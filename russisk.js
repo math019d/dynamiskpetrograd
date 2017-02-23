017d3a419679e49569ef8f6c57638b63e5ae64c3
Mathias Nybo <Mathias@tobiaslenovo.kea.local>
2017-02-23 13:28:07 +0100

    load JSON produktliste and create template clone for each

--- russisk.js
+++ russisk.js
@@ -3,13 +3,24 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("siden vises");
 
-    visProdukt();
+
+    // læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
 
 
 
 }
 
-function visProdukt() {
+function visProduktListe(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+
+}
+
+
+function visProdukt(produkt) {
+    console.log(produkt);
     //klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
     // indsæt data i klon
